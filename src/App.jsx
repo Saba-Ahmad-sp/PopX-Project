fd9b6b685e55f1712f803b79caf59aa6cbb158ec
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-05-13 13:28:21 +0530

    Adds initial files for PopX Project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Account from "./components/Account";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <SignUp />
+      <Login />
+      <Account />
+    </>
+  );
+}
+
+export default App;
